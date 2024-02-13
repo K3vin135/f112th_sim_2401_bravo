013eb7e5ba2b1fda47a812810770eb037087abce
Sebastian <gomez.sebastian2310@gmail.com>
2024-02-12 22:44:51 -0500

    Added twist mux

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -47,10 +47,27 @@ def generate_launch_description():
         package='controller_manager', 
         executable='spawner', 
         arguments=['joint_broad'])
+    
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    
+    twist_mux_node = Node(package='twist_mux', 
+                    executable='twist_mux',
+                    parameters=[twist_mux_params,{'use_sim_time': True}],
+                    remappings=[('/cmd_vel_out','/ack_cont/reference_unstamped')]
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
+        twist_mux_node,
         gazebo,
         spawn_entity,
 		ack_drive_spawner,
